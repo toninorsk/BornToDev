b3f8e9b8c6657221ab7b906038eb838309145474
Nattawoot <nattawoot.denduangchai@gmail.com>
2020-09-21 23:14:58 +0700

    first commit

---
+++ dbManager.py
@@ -0,0 +1,8 @@
+import sqlite3
+
+connection = sqlite3.connect('chinook.db')
+print("Connected to DB successfully!")
+cursor = connection.execute("SELECT * FROM Customers;")
+for row in cursor: 
+    print("Customer ID :", row[0], row[1], row[2])
+connection.close()
